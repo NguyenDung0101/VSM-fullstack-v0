38077b4cff7a87f391abf44b208ebee183162e77
Superherocode <213042559+NguyenDung0101@users.noreply.github.com>
2025-09-15 18:05:02 +0700

    com

--- app/admin/settings/page.tsx
+++ app/admin/settings/page.tsx
@@ -3,7 +3,6 @@
 import { Badge } from "@/components/ui/badge";
 
 import { useEffect, useState } from "react";
-import { DashboardLayout } from "@/components/dashboard/dashboard-layout";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
